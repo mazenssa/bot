da100ba7234c6bb7cab644918da4b6c0f3a093c6
mazenssa <46714246+mazenssa@users.noreply.github.com>
2019-01-19 00:54:00 +0200

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,13 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+console.log("Scrpit By Dream");
+
+
+client.on("ready", () => {
+let channel =     client.channels.get("534484425300508696")
+setInterval(function() {
+channel.send(`hell gays hello mazen ana esmy mazen ayman mohamed elsayed mazen  hello`);
+}, 30)
+})
+
+client.login("NTM1Mzg5Mzg5OTY3MDY1MDg4.DyPm_g.TrIQCtzhy0pBCekvO-BQMyX-SX0");
