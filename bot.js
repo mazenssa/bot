c2d37bb550b4c2c7bc32cc1100d4852c9ba40f3a
mazenssa <46714246+mazenssa@users.noreply.github.com>
2019-01-19 01:02:48 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -6,7 +6,8 @@ console.log("Scrpit By Dream");
 client.on("ready", () => {
 let channel =     client.channels.get("534484425300508696")
 setInterval(function() {
-channel.send(`hell gays hello mazen ana esmy mazen ayman mohamed elsayed mazen  hellogjgnfhjkghkhjhfhd hhkhflksdhfishidfuhskdfdsh hyueyruiwyrhkdshfkhdskfhye w iuyehuiyhuihdh  wu yrweyiro73yieudfsuhfeuyfu eywuirywyr7w8reuwifhyhdfksjhfkshdkdflg heuhreiuthehgtrehfgheuiehgfhifdhgirey uewruiwheiurhweiuyhr uhewrhyuewhr ue r hruehrfuherfewu wheurfewuyrhf uh ewuryehwufyrudyfewhufywe uweuewyr`);
+channel.send(`hell gays hello mazen ana esmy mazen ayman mohamed elsayed mazen 
+hello brgsd bye-bye`);
 }, 30)
 })
 
